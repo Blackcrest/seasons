7f9a13b858480a9d9fd7a9bc4febf05f2f5e50a5
Patrick Lassche <p.lassche318@gmail.com>
2019-09-12 21:02:24 +0200

    First few videos in

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+    // Good place to do one-time setup
+    constructor(props) {
+        super(props);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        this.state = {
+            lat: null,
+            errorMessage: ''
+        }
+    }
+
+    // Good place to do data loading
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({lat: position.coords.latitude}),
+            err => this.setState({errorMessage: err.message})
+        );
+    }
+
+    // Good place to do more data loading when state/props change
+    componentWillUpdate(){
+        console.log("My component was just updated - it rerendered");
+    }
+
+    // Good place to clean up (especially for non-React stuff)
+    // componentWillUnmount(){
+    //     console.log("My component is now unmounted");
+    // }
+
+    // Just let this render JSX
+    render() {
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if(!this.state.errorMessage && this.state.lat){
+            return <div>Latitude: {this.state.lat}</div>
+        }
+
+        return <div>Loading</div>
+
+    }
+}
+
+ReactDOM.render(
+    <App />, document.querySelector('#root')
+)
